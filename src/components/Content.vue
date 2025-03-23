f7421899a03bca81e878bb8ada94f8246872b477
Alejandro juarez <calen11708@hotmail.com>
2025-03-23 14:07:42 -0600

    init

---
+++ src/components/Content.vue
@@ -0,0 +1,9 @@
+<script></script>
+
+<template>
+	<main>
+		<h1>Penis in your butt</h1>
+	</main>
+</template>
+
+<style></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<script></script>
+
+<template>
+	<header>
+		<nav><a href="#">Home</a> | <a href="#">Link</a></nav>
+	</header>
+</template>
+
+<style></style>
